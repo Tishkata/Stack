5248f5aaafb464ef0cb68bbe0478dcf727f55c90
Tihomir Kovachev <tihomir.kovachev@egt-bg.com>
2018-03-15 12:21:57 +0200

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include "stack.h"
+
+// task
+// Read from txt file and put every symbol in stack except *.
+void decrypt(const char* filename)
+{
+    ifstream ifs;
+    ifs.open(filename, ios::in);
+    if(!ifs.is_open())
+    {
+        cerr << "Could not open file." << endl;
+        return;
+    }
+
+    Stack<char> input;
+    char symbol;
+    while(true)
+    {
+        ifs >> symbol;
+        if(symbol != '*')
+        {
+            if(ifs.eof())
+            {
+                break;
+            }
+
+            input.push(symbol);
+        }
+        else if(symbol == '*')
+        {
+            if(ifs.eof())
+            {
+                break;
+            }
+            cout << input.top();
+            input.pop();
+        }
+    }
+}
+
+int main()
+{
+    // task
+    const char* filename = "Text File";
+    decrypt(filename);
+
+    /** Creating a stack of int type, in this case we call the default constructor. */
+    Stack<int> myStack;
+
+    /** adding elements into stack */
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+    myStack.push(4);
+    myStack.push(5);
+
+    /** print stack */
+    cout << "Stack elements: ";
+    myStack.print();
+    cout << endl;
+
+    /** Creating a new stack with the same values as the one above, in this case we call the Copy Constructor. */
+    Stack<int> copyStack(myStack);
+
+    /** print copied stack */
+    cout << "Copied stack elements: ";
+    copyStack.print();
+    cout << endl;
+
+    /** Creating new stack with assignment operator, almost the same as the Copy Constructor */
+    Stack<int> assignmentStack = copyStack;
+
+    /** Print assignment stack */
+    cout << "Assignment stack elements: ";
+    assignmentStack.print();
+    cout << endl;
+
+    /** top element of stack */
+    cout << "Stack top element: " << assignmentStack.top() << endl;
+
+    /** remove element of stack, in this case we remove 5 */
+    assignmentStack.pop();
+    cout << "New stack elements: ";
+    assignmentStack.print();
+    cout << endl;
+
+    return 0;
+}
---
+++ stack.h
@@ -0,0 +1,156 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+template <typename T>
+struct Node
+{
+    T data;
+    Node<T>* previous;
+    Node(T _data, Node<T>* _previous = nullptr):data(_data), previous(_previous){}
+};
+
+template <typename T>
+class Stack
+{
+// the public constructors and operators.
+public:
+    Stack();
+    Stack(const Stack& other);
+    Stack<T>& operator =(const Stack &other);
+    ~Stack();
+
+// the public methods
+public:
+    bool isEmpty();
+    void push(const T& data);
+    void pop();
+    T& top();
+    size_t size();
+    void print();
+
+// the private methods
+private:
+    void destroy();
+    void copy(const Stack& other);
+
+// the private members
+private:
+    Node<T>* m_top;
+    size_t m_size;
+
+};
+
+template <typename T>
+void Stack<T>::destroy()
+{
+    while(!isEmpty())
+    {
+        this->pop();
+    }
+}
+
+template <typename T>
+void Stack<T>::copy(const Stack& other)
+{
+    m_size = other.m_size;
+    Node<T> *ours, *theirs;
+    m_top = new Node<T>(other.m_top->data);
+    ours = m_top;
+    theirs = other.m_top->previous;
+    while(theirs)
+    {
+        ours->previous = new Node<T>(theirs->data);
+        ours = ours->previous;
+        theirs = theirs->previous;
+    }
+}
+
+template <typename T>
+Stack<T>::Stack():m_top(nullptr), m_size(0)
+{
+
+}
+
+template <typename T>
+Stack<T>::Stack(const Stack& other)
+{
+    if(this != &other)
+    {
+        copy(other);
+    }
+}
+
+template <typename T>
+Stack<T>& Stack<T>::operator =(const Stack& other)
+{
+    if(this != &other)
+    {
+        copy(other);
+    }
+    return* this;
+}
+
+template <typename T>
+Stack<T>::~Stack()
+{
+    destroy();
+}
+
+template <typename T>
+bool Stack<T>::isEmpty()
+{
+    return this->m_size == 0;
+}
+
+template <typename T>
+void Stack<T>::push(const T& data)
+{
+    Node<T>* temp = new Node<T>(data);
+    temp->previous = this->m_top;
+    this->m_top = temp;
+    this->m_size++;
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    if(isEmpty())
+    {
+        return;
+    }
+    Node<T>* temp;
+    temp = this->m_top;
+    this->m_top = this->m_top->previous;
+    delete[] temp;
+    this->m_size--;
+}
+
+template <typename T>
+T& Stack<T>::top()
+{
+    return this->m_top->data;
+}
+
+template <typename T>
+size_t Stack<T>::size()
+{
+    return this->m_size;
+}
+
+template <typename T>
+void Stack<T>::print()
+{
+    Node<T>* temp = m_top;
+    while(temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->previous;
+    }
+}
+
+#endif // STACK_H
